2df9a3061c7ebc2c982580679a8037954bfe88e8
Sashank Thupukari <sthupukari@gmail.com>
2014-09-13 05:25:27 -0400

    Convert Street Address to Lat and Lgt
    Using Google Geocaching API
    Add HTTP package

--- net-neutrality.js
+++ net-neutrality.js
@@ -1,9 +1,38 @@
 if (Meteor.isClient) {
 
+    Template.contactInfo.events({
+        'click #submitContactInfo': function() {
+            firstName = $("#firstName").val();
+            lastName = $("#lastName").val();
+            streetAddress = $("#streetAddress").val();
+            zipCode = $("#zipCode").val();
+
+            var contact = {};
+            contact["firstName"] = firstName;
+            contact["lastName"] = lastName;
+            contact["streetAddress"] = streetAddress;
+            contact["zipCode"] = zipCode;
+
+
+            Meteor.call('convertStreetAddress', streetAddress, function(e, r) {
+                locationObject = r;
+            });
+        }
+    });
 }
 
 if (Meteor.isServer) {
-    Meteor.startup(function() {
-        // code to run on server at startup
+    Meteor.methods({
+        convertStreetAddress: function(streetAddress) {
+            var GOOGLEAPIKEY = "YOUR API KEY";
+            var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + streetAddress + "&key=" + GOOGLEAPIKEY;
+            //synchronous GET
+            var result = HTTP.call("GET", url);
+
+            toEval = "var x=" + result.content;
+            eval(toEval);
+            return x.results[0].geometry.location;
+
+        },
     });
 }
